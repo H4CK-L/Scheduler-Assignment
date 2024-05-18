6a178e9942c7a65afd4b07669cfae7b45ba58ccd
H4CK-L <dltowls000@gmail.com>
2024-05-18 17:34:04 +0900

    FCFS 완성 및 SJF 생성

--- src/FCFS.java
+++ src/FCFS.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
 
 public class FCFS {
     private ArrayList<Process> processes = new ArrayList<Process>();
