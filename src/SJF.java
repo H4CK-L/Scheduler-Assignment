65e90245ac3ce0487f5e1d37b9f57eceb8e616d0
NoImpMe <hn00428@naver.com>
2024-05-18 17:42:19 +0900

    민우

---
+++ src/SJF.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SJF {
+    private ArrayList<Process> processes = new ArrayList<Process>();
+
+    public SJF(ArrayList<Process> processes) {
+        this.processes = processes;
+    }
+
+
+}
