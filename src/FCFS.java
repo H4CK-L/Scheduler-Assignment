395432635b4795afad2b6d41966a416878d6aec0
H4CK-L <dltowls000@gmail.com>
2024-05-18 17:23:23 +0900

    FCFS 완성 및 SJF 생성

--- src/FCFS.java
+++ src/FCFS.java
@@ -8,6 +8,7 @@ public class FCFS {
     private ArrayList<Integer> keySet;
     private int allTime = 0;
     private boolean checkFirst = false;
+    private double totalWaitTime = 0;
 
     public FCFS(ArrayList<Process> processes) {
         this.processes = processes;
@@ -33,18 +34,22 @@ public class FCFS {
             if(!checkFirst){
                 processes.get(keySet.get(key)).setTime(processes.get(key).getRequiredCpuTime(), 0, 0);
                 allTime += processes.get(key).getRequiredCpuTime();
+                System.out.println(processes.get(key).getWaitingTime());
                 checkFirst = true;
                 continue;
             }
             processes.get(keySet.get(key)).setTime(processes.get(key).getRequiredCpuTime(), allTime - hs.get(key), processes.get(key).getRequiredCpuTime());
             allTime += processes.get(key).getRequiredCpuTime();
+            System.out.println(processes.get(key).getWaitingTime());
         }
         for(int key : keySet){
             System.out.println("프로세스 " + key + " 의 대기시간 : " + processes.get(key).getWaitingTime());
+            totalWaitTime += processes.get(key).getWaitingTime();
         }
         System.out.println("총 실행 : " + allTime);
-        System.out.println("평균 대기시간 : ");
+        System.out.println("평균 대기시간 : " + totalWaitTime / processes.size());
         allTime = 0;
+        totalWaitTime = 0;
         checkFirst = false;
     }
 }
