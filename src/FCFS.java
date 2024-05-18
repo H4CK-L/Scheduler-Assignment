be33f52a6a0dfbf8f174139d8fb0945d4a24d3bf
H4CK-L <dltowls000@gmail.com>
2024-05-18 17:03:26 +0900

    asd

--- src/FCFS.java
+++ src/FCFS.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class FCFS {
+    private ArrayList<Process> processes = new ArrayList<Process>();
+    private HashMap<Integer, Integer> hs = new HashMap<Integer, Integer>();
+    private ArrayList<Integer> keySet;
+    private int allTime = 0;
 
+    public FCFS(ArrayList<Process> processes) {
+        this.processes = processes;
+        setArrive();
+    }
 
+    private void setArrive(){
+        for(int i = 0; i < processes.size(); i++){
+            hs.put(processes.get(i).getPsNumber(), processes.get(i).getArriveTime());
+        }
+        keySet = new ArrayList<>(hs.keySet());
+
+        keySet.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return hs.get(o1).compareTo(hs.get(o2));
+            }
+        });
+    }
+
+    public void run(){
+        for(int key : keySet){
+            if(key == 0){
+                processes.get(keySet.get(key)).setTime(processes.get(key).getRequiredCpuTime(), 0, 0);
+                allTime += processes.get(key).getRequiredCpuTime();
+                continue;
+            }
+            allTime += processes.get(key).getRequiredCpuTime();
+            processes.get(keySet.get(key)).setTime(processes.get(key).getRequiredCpuTime(), processes.get(key-1).getRequiredCpuTime() - processes.get(key).getRequiredCpuTime(), processes.get(key).getRequiredCpuTime());
+        }
+        for(int key : keySet){
+            System.out.println("프로세스 " + key + " 의 대기시간 : " + processes.get(key-1).getWaitingTime());
+        }
+        System.out.println("총 실행 : " + allTime);
+        System.out.println("평균 대기시간 : ");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,44 @@ public class Main {
         int arrive;
         int priority;
         ArrayList<Process> ps = new ArrayList<Process>();
+
         for(int i = 0; i < num; i++){
             System.out.println("Burst Time, Arrive Time, Priority를 순서대로 입력하시오.");
             burst = psNum.nextInt();
             arrive = psNum.nextInt();
             priority = psNum.nextInt();
 
-            Process ps1 = new Process(i+1, burst, arrive, priority);
+            Process ps1 = new Process(i, burst, arrive, priority);
             ps.add(ps1);
             System.out.println("프로세스 생성 완료");
+        }
+        int a = 1;
+        FCFS fcfs = new FCFS(ps);
+       // HRRN hrrn = new HRRN(ps);
+       // RoundRobin rr = new RoundRobin(ps);
+        //SJF sjf = new SJF(ps);
+        while(a != 0){
+            System.out.println("실행할 스케줄러 선택(1 : FCFS, 2 : SJF, 3 : HRRN, 4 : RR, 0 : 종료 | 입력 : ");
+            a = psNum.nextInt();
+            if(a == 1){
+                fcfs.run();
+            }
+            else if(a == 2){
+
+            }
+            else if(a == 3){
+
+            }
+            else if(a == 4){
+
+            }
+            else if(a == 0){
+                System.out.println("종료");
+            }
+            else{
+                System.out.println("재입력");
+                continue;
+            }
         }
     }
 }
