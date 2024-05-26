638fe89ed3572662ac1db6a1698735982159d280
minu <hn00428@naver.com>
2024-05-26 17:34:24 +0900

    newscheduling

--- src/HRRN.java
+++ src/HRRN.java
@@ -2,77 +2,96 @@ import java.util.*;
 
 public class HRRN {
     private ArrayList<Process> ps;
-    private HashMap<Double, Integer> hs =new HashMap<>();
-    private ArrayList<Double> keyset;
+    private HashMap<Integer, Double> hs = new HashMap<>();
+    private ArrayList<Integer> keyset;
     private int allTime = 0;
-    public HRRN(ArrayList<Process> ps){
-        this.ps=ps;
-    }
+    private int[] waitingTimePerProcess;
 
-    public void setPriority(){
-        if(!ps.isEmpty()){
-            for(int i = 0 ; i < ps.size() ; i++){
-                if(ps.get(i).preempt){
-                    if(ps.get(i).getRequiredCpuTime() == 0){
-                        hs.remove(ps.get(i).getResponseRatio());
-                        ps.remove(i);
-                        i--;
-                    }
-                    else {
-                        ps.get(i).setResponseRatio();
-                        hs.put(ps.get(i).getResponseRatio(), ps.get(i).getPsNumber());
-                    }
-                }
+    public HRRN(ArrayList<Process> ps) {
+        this.ps = ps;
+        for (Process p : ps) {
+            p.setPriority(1000);
+        }
+        waitingTimePerProcess = new int[ps.size()];
+    }
+    public boolean setting(){
+        for(Process p : ps) {
+            if(p.preempt){
+                return true;
             }
-            if(!hs.isEmpty()) {
-                keyset = new ArrayList<>(hs.keySet());
-                keyset.sort((o1, o2) -> o2.compareTo(o1));
-                int pri = 0;
-                for (Double key : keyset) {
-                    ps.get(hs.get(key)).setPriority(pri++);
+        }
+        return false;
+    }
+    public void setPriority() {
+        if (!ps.isEmpty()) {
+            for (int i = 0; i < ps.size(); i++) {
+                if (ps.get(i).preempt) {
+                    ps.get(i).setWaitingTime(allTime - ps.get(i).getArriveTime());
+                    ps.get(i).setResponseRatio();
+                    hs.put(ps.get(i).getPsNumber(), ps.get(i).getResponseRatio());
                 }
             }
         }
-
-    }
-
-    public void running(){
-        for (Double key : keyset) {
-            System.out.println("key :" + key + "value :" + hs.get(key) );
-            if(!ps.get(hs.get(key)).preempt){
-                ps.get(hs.get(key)).increaseTime();
-                hs.remove(key);
+        if (!hs.isEmpty()) {
+            this.keyset = new ArrayList<>(hs.keySet());
+            keyset.sort(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return hs.get(o2).compareTo(hs.get(o1));
+                }
+            });
+            for (int i = 0; i < ps.size(); i++) {
+                ps.get(i).setPriority(keyset.indexOf(ps.get(i).getPsNumber()));
             }
-            else{
-                ps.get(hs.get(key)).onJob();
-                hs.remove(key);
-            }
-
         }
-
-        allTime++;
     }
-//arrived time 0이 안왔을때 종료가 안되게 하는 법
-    public void run(){
-        while(true){
 
-            if(ps.size() == 0) {
+    public boolean running() {
+        if (!hs.isEmpty()) {
+            for (int i = 0; i < ps.size(); i++) {
+                if (ps.get(i).getPriority() == 0) {
+
+                    if (ps.get(i).getResponseTime() < 0) {
+                        ps.get(i).setResponseTime(allTime);
+                    }
+                    while (ps.get(i).getRequiredCpuTime() > 0) {
+                        ps.get(i).onJob();
+                        allTime++;
+                    }
+                    ps.get(i).setTurnaroundTime(allTime - ps.get(i).getArriveTime());
+                    waitingTimePerProcess[i] = ps.get(i).getWaitingTime();
+                    System.out.println("PS[" + (ps.get(i).getPsNumber()+1) + "] 의 Turnaround Time = " + ps.get(i).getTurnaroundTime() + " Response Time = " + ps.get(i).getResponseTime() + " Waiting Time = " + ps.get(i).getWaitingTime());
+                    ps.remove(i);
+                    i--;
+                }
+            }
+            hs.clear();
+            return true;
+        }
+        return false;
+    }
+
+
+    public void run() {
+
+        while (true) {
+            if (ps.size() == 0) {
                 System.out.println("HRRN종료");
                 break;
             }
-            for(Process p : ps) {
+            for (Process p : ps) {
                 if (p.getArriveTime() <= allTime) {
                     p.preempt = true;
                 }
-                if (p.getPriority() != 0) {
-                    p.preempt = false;
-                }
             }
-            setPriority();
-            running();
-
-
-
+            if(setting()){
+                setPriority();
+                running();
+            }
+            else{
+                allTime++;
+            }
         }
+
     }
 }
