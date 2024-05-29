4522c8ce130bcc3043bdcdcaaa128f86121ca772
H4CK-L <dltowls000@gmail.com>
2024-05-29 14:33:39 +0900

    FCFS 재구현 시작

--- src/FCFS.java
+++ src/FCFS.java
@@ -2,42 +2,51 @@ import java.util.*;
 
 public class FCFS {
     private ArrayList<Process> processes = new ArrayList<Process>();
-    private HashMap<Integer, Integer> hs = new HashMap<Integer, Integer>();
-    private ArrayList<Integer> sKey;
+    ArrayList<Process> readyQueue = new ArrayList<>();
+    private int allTime = 0;
+    private int waitTime = 0;
+    private int totalWaitTime = 0;
 
     public FCFS(ArrayList<Process> processes) {
         this.processes = processes;
-        setArrive();
-    }
-
-    private void setArrive(){
-        for(int i = 0; i < processes.size(); i++){
-            hs.put(processes.get(i).getPsNumber(), processes.get(i).getArriveTime());
-        }
-        sKey = new ArrayList<>(hs.keySet());
-
-        sKey.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return hs.get(o1).compareTo(hs.get(o2));
-            }
-        });
     }
 
     public void run(){
-        int allTime = 0;
-        double totalWaitTime = 0;
-        boolean checkFirst = false;
-        for(int key : sKey){
-            if(!checkFirst){
-                processes.get(key).setTime(processes.get(key).getRequiredCpuTime(), processes.get(key).getArriveTime(), processes.get(key).getArriveTime());
-                allTime += processes.get(key).getRequiredCpuTime();
-                checkFirst = true;
-                continue;
+        Process p;
+        for(int i = 0; i < processes.size(); i++){
+            p = processes.get(i);
+            for(int j = i + 1; j < processes.size(); j++){
+                if(p.getArriveTime() > processes.get(j).getArriveTime()){
+                    p = processes.get(j);
+                }
             }
-            processes.get(key).setTime(processes.get(key).getRequiredCpuTime(), allTime - hs.get(key), allTime - hs.get(key));
-            allTime += processes.get(key).getRequiredCpuTime();
+            readyQueue.add(p);
         }
+
+        while(!readyQueue.isEmpty()){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         System.out.println("============================");
         for(Process p : processes){
